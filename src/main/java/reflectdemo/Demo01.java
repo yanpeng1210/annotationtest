aaaae6e800f85dcf2614fa2fac5a618a99bf6d5a
闫鹏github <476059272@qq.com>
2018-10-25 14:30:29 +0800

    update

--- src/main/java/reflectdemo/Demo01.java
+++ src/main/java/reflectdemo/Demo01.java
@@ -1,5 +1,16 @@
 package reflectdemo;
 
 public class Demo01 {
+    public static void main(String[] args) {
+        String path = "reflectdemo.User";
+        try {
+            Class clazz = Class.forName(path);
+            System.out.println(clazz.hashCode());
+            Class clazz2 = Class.forName(path);
+            System.out.println(clazz2.hashCode());//一个类只对应一个class对象
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
